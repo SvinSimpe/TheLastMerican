1f536824cba1046c7cc4ce5abeedb33ffd428a7a
unknown <sebastiangenlund@hotmail.com>
2015-06-26 20:02:54 +0200

    Added state machine Cleaned game.cs from comments

---
+++ TheLastMerican/TheLastMerican/States/BaseState.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheLastMerican.States
+{
+    static public class GameStateType
+    {
+        static public int NumberOfStates    = 1;
+
+        // ----------- Add new states here -------------
+        static public int PLAY_STATE                = 0;
+        // ---------------------------------------------
+    }
+
+    abstract class BaseState
+    {
+        protected ContentManager    mContent;
+        protected int               mStateType;
+
+        public int StateType
+        {
+            get { return mStateType; }
+            set { mStateType = value; }
+        }
+
+        public BaseState(ContentManager content)
+        {
+            mContent    = content;
+        }
+
+        virtual public void Update( GameTime gameTime )
+        {}
+
+        virtual public void Draw( SpriteBatch spriteBatch )
+        {}
+
+        virtual public void OnEnter()
+        {}
+
+        virtual public void OnExit()
+        {}
+
+        virtual public void Reset()
+        {}
+    }
+}
